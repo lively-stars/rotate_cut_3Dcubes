86fbfcce1691f669ca421d7bdb21dae30d6febd7
veronika.witzke <witzke@mps.mpg.de>
2024-06-23 14:42:11 +0200

    intermediate_test_moving

--- src/rotate.F90
+++ src/rotate.F90
@@ -45,19 +45,19 @@
       do i = 1, Nx
         do k = 1, Ny
            do j = 1, Nzcut 
-             newx = (k-1)*dx - pivotdx +(j-1)*newdx 
+             newx = (i-1)*dx - pivotdx +(j-1)*newdx 
              newz = (j-1)*(dz)*mu;
 
              if (newx .lt. 0.0) then 
-                  newx = newx - floor(newx/(dble(Ny)*dx)) * (Ny*dx)  
+                  newx = newx - floor(newx/(dble(Nx)*dx)) * (Nx*dx)  
                   do while( newx .lt. 0.0)  
-                    newx = newx+(Ny*dx)
+                    newx = newx+(Nx*dx)
                   enddo 
                  
-             elseif (newx .ge. (Ny*dx)) then 
-                  newx = newx - (floor(newx/(dble(Ny)*dx))*(Ny*dx))  
-                  do while(newx .ge. (Ny*dx))  
-                    newx = newx - (floor(newx/(dble(Ny)*dx))*(Ny*dx))   
+             elseif (newx .ge. (Nx*dx)) then 
+                  newx = newx - (floor(newx/(dble(Nx)*dx))*(Nx*dx))  
+                  do while(newx .ge. (Nx*dx))  
+                    newx = newx - (floor(newx/(dble(Nx)*dx))*(Nx*dx))   
                   enddo 
              endif 
 
@@ -70,21 +70,23 @@
              z_l = z_l +1
              xn = x_l + 1
              xnp = xn+1 
-             if (xnp .gt. Ny) xnp = mod(xnp, Ny)  
+             if (xnp .gt. Nx) xnp = mod(xnp, Nx)  
 
-             newT(i,k,j)  = ((T(i, xn, z_l)) * (1.0d0-x_f) + x_f*(T(i, xnp,  z_l))) *(1.0d0- z_f)
-             newT(i,k,j)  = newT(i,k,j) +  z_f*((T(i, xn, z_l+1))*(1.0d0-x_f)+ x_f*(T(i, xnp,z_l+1)))
+             newT(i,k,j)  = ((T(xn, k, z_l)) * (1.0d0-x_f) + x_f*(T(xnp, k,  z_l))) *(1.0d0- z_f)
+             newT(i,k,j)  = newT(i,k,j) +  z_f*((T(xn, k, z_l+1))*(1.0d0-x_f)+ x_f*(T(xnp, k,z_l+1)))
 
-             newP(i,k,j)  = ((P(i, xn, z_l)) * (1.0d0-x_f) + x_f*(P(i, xnp,  z_l))) *(1.0d0- z_f)
-             newP(i,k,j)  = newP(i,k,j) +  z_f*((P(i, xn, z_l+1))*(1.0d0-x_f)+ x_f*(P(i, xnp,z_l+1)))
+             newP(i,k,j)  = ((P(xn, k, z_l)) * (1.0d0-x_f) + x_f*(P(xnp, k,  z_l))) *(1.0d0- z_f)
+             newP(i,k,j)  = newP(i,k,j) +  z_f*((P(xn, k, z_l+1))*(1.0d0-x_f)+ x_f*(P(xnp, k,z_l+1)))
 
+             newrho(i,k,j)  = ((rho(xn, k, z_l)) * (1.0d0-x_f) + x_f*(rho(xnp, k,  z_l))) *(1.0d0- z_f)
+             newrho(i,k,j)  = newrho(i,k,j) +  z_f*((rho(xn, k, z_l+1))*(1.0d0-x_f)+ x_f*(rho(xnp, k,z_l+1)))
 
-             newrho(i,k,j)  = ((rho(i, xn, z_l)) * (1.0d0-x_f) + x_f*(rho(i, xnp,  z_l))) *(1.0d0- z_f)
-             newrho(i,k,j)  = newrho(i,k,j) +  z_f*((rho(i, xn, z_l+1))*(1.0d0-x_f)+ x_f*(rho(i, xnp,z_l+1)))
 
 #ifdef VELO
-             newVtot(i,k,j)  = ((Vtot(i, xn, z_l)) * (1.0d0-x_f) + x_f*(Vtot(i, xnp,  z_l))) *(1.0d0- z_f)
-             newVtot(i,k,j)  = newVtot(i,k,j) +  z_f*((Vtot(i, xn, z_l+1))*(1.0d0-x_f)+ x_f*(Vtot(i, xnp,z_l+1)))
+
+             newVtot(i,k,j)  = ((Vtot(xn, k, z_l)) * (1.0d0-x_f) + x_f*(Vtot(xnp, k,  z_l))) *(1.0d0- z_f)
+             newVtot(i,k,j)  = newVtot(i,k,j) +  z_f*((Vtot(xn, k, z_l+1))*(1.0d0-x_f)+ x_f*(Vtot(xnp, k,z_l+1)))
+
 #endif 
 
             znew = zold + dz*(j-1)*1.0d-5 
@@ -111,8 +113,8 @@
    integer  xn, xnp, x_l
 
    real(kind=8), intent(in) :: mu, pivot, dx, dy, dz
-   real(kind=8)  theta, pivotdx,  newdy, newy, newz
-   real(kind=8) phi , pi_n
+   real(kind=8)  theta, pivotdx, pivotdy,  newdy, newy, newz
+   real(kind=8) phi , pi_n, cosphi
    real(kind=8)  newdx, newx
 
    real(kind=8)   z_f,  y_f, x_f
@@ -123,11 +125,19 @@
 
    pi_n = 4.0*atan(1.d0)
    phi = pi_n/180.0d0*phi
+   cosphi = cos(phi) 
+   if (abs(cosphi) .lt. 1.0d-10) cosphi = 0.0d0
 
    theta = acos(mu)
-   pivotdx = tan(theta) * pivot
-   newdy = dz * sin(theta) *cos(phi)
+   pivotdy = tan(theta) * pivot * cosphi
+   pivotdx = tan(theta) * pivot *sin(phi) 
+
+   newdy = dz * sin(theta) *cosphi
    newdx = dz *sin(theta)*sin(phi)
+
+
+   print*, ' pivotdy, newdy ', pivotdy, newdy
+   print*, ' pivotdx, newdx ', pivotdx, newdx 
 
    Nzcut = Nz
    if (mu .le. 0.9) Nzcut = int(Nz*(0.9d0/mu))
@@ -150,17 +160,16 @@
       do i = 1, Nx
         do k = 1, Ny
            do j = 1, Nzcut
-             newy = (k-1)*dy - pivotdx +(j-1)*newdy
+             newy = (k-1)*dy - pivotdy +(j-1)*newdy
              newx = (i-1)*dx - pivotdx + (j-1)*newdx
 
              newz = (j-1)*(dz)*mu;
 
              if (newy .lt. 0.0) then
-                  newy = newy - floor(newy/(dble(Ny)*dy)) * (Ny*dy)
+                 ! newy = newy - floor(newy/(dble(Ny)*dy)) * (Ny*dy)
                   do while( newy .lt. 0.0)
                     newy = newy+(Ny*dy)
                   enddo
-
              elseif (newy .ge. (Ny*dy)) then
                   newy = newy - (floor(newy/(dble(Ny)*dy))*(Ny*dy))
                   do while(newy .ge. (Ny*dy))
@@ -169,12 +178,12 @@
              endif
 
              if (newx .lt. 0.0) then
-                  newx = newx - floor(newx/(dble(Nx)*dx)) * (Nx*dx)
+                 ! newx = newx - floor(newx/(dble(Nx)*dx)) * (Nx*dx)
                   do while( newx .lt. 0.0)
                     newx = newx+(Nx*dx)
                   enddo
 
-             elseif (newx .ge. (Ny*dx)) then
+             elseif (newx .ge. (Nx*dx)) then
                   newx = newx - (floor(newx/(dble(Nx)*dx))*(Nx*dx))
                   do while(newx .ge. (Nx*dx))
                     newx = newx - (floor(newx/(dble(Nx)*dx))*(Nx*dx))
